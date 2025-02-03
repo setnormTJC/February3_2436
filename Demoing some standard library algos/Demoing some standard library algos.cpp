90b231d2047ed459934edcc3c8e4658e8a9017c4
Work <Work@MSI>
2025-02-03 13:09:08 -0600

    Add project files.

---
+++ Demoing some standard library algos/Demoing some standard library algos.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::cout, std::vector, std::string; 
+
+bool stringLengthIsGreaterThan3(const string& theString)
+{
+    return (theString.length() > 3);
+}
+
+
+int main()
+{
+    vector<string> names =
+    {
+        "Alice",
+        "Bob",
+        "Carol",
+        "Darth", 
+        "asdfasdfasdfasdf", 
+        "123"
+    };
+
+    //suppose I want to print the number of names with length > 3:
+    cout << "Number of names with len > 3: " 
+        << std::count_if(names.begin(), names.end(), stringLengthIsGreaterThan3) << "\n";
+
+    
+
+    return 0;
+}
